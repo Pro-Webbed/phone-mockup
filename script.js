19c75e4dbc368f791d170a1216f4f750905224b0
TeenCoder159 <no1umang@gmail.com>
2025-01-20 01:06:03 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function smoothScrolling(id) {
+    document.getElementById(id).scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+
+function mockup(){
+    alert("This website is purely a mockup and has little to no functonality")
+}
